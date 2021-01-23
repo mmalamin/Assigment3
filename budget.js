e4dcc5b298b87265ea89111f28acfe07a6fba8e7
MM Al-Amin <mmalamin078@gmail.com>
2021-01-23 23:20:58 +0600

    first javascript

--- budget.js
+++
@@ -1,8 +0,0 @@
-// amr kace besi tk nai vai tai ekta korei kinlam
-
-function budgetCalculator(clock,phone, laptop){
-var result = clock * 3 + phone * 6 + laptop * 8;
-    return result;
-}
-var totalCost = budgetCalculator(100,8000,30000);
-console.log(totalCost);
